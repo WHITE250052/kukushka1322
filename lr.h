92f8ffc84d2007d1be932f44a05404d96c3426d7
WHITE250052 <57279148+WHITE250052@users.noreply.github.com>
2024-05-22 20:05:52 +0300

    Add files via upload

---
+++ lr.h
@@ -0,0 +1,16 @@
+#ifndef LR_H_INCLUDED
+#define LR_H_INCLUDED
+int isVowel(char c);
+int lab1();
+int lab11();
+int lab2();
+int lab21();
+int lab3();
+int lab4();
+int lab41();
+int lab5();
+int lab51();
+int lab6();
+int lab61();
+int lab7();
+#endif
